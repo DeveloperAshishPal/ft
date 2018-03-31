81b5de3208a4b2a852f60b48e84e4852cd4925a5
Ashish Pal <developerashishpal@gmail.com>
2018-03-31 14:28:40 +0530

    up

--- client/assets/js/clientUpgrade.js
+++
@@ -1,4 +0,0 @@
-angular.module('ClientMaster.upgrade', []).controller('UpgradeCtrl', ['$scope', '$http', '$state','$location', function ($scope, $http, $state,$location) {
-    console.log('client upgrade Controller is working!!!!!!!');
-
-}]);
